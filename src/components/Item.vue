f9ba1b1489896688eef2ded1047baa690772a1e3
Postnov Daniil <dan.postnov@gmail.com>
2019-02-19 10:34:30 +0300

    add clear fields

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -26,9 +26,13 @@
             <svg
                 @click="$emit('removeItem', id)"
                 class="cross" viewBox="0 0 24 24">
+
                 <path xmlns="http://www.w3.org/2000/svg" d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"></path>
             </svg>
-            <svg class="reset" viewBox="0 0 24 24">
+            <svg
+                @click="clearFields()"
+                class="reset" viewBox="0 0 24 24">
+
                 <path d="M12 0c3.31 0 6.291 1.353 8.459 3.522l2.48-2.48 1.061 7.341-7.437-.966 2.489-2.489c-1.808-1.807-4.299-2.928-7.052-2.928-5.514 0-10 4.486-10 10s4.486 10 10 10c3.872 0 7.229-2.216 8.89-5.443l1.717 1.046c-2.012 3.803-6.005 6.397-10.607 6.397-6.627 0-12-5.373-12-12s5.373-12 12-12z"/>
             </svg>
         </div>
@@ -68,6 +72,14 @@ export default {
                 costTotal: costTotal || 0,
                 id
             });
+        },
+
+        clearFields() {
+            this.costPerClient = '';
+            this.cost          = '';
+            this.label         = '';
+            this.value         = '';
+            this.costTotal     = 0;
         }
     },
     updated() {
